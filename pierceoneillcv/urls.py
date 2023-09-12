60171637c3dc323521829d106a9b24057f2eb3ed
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-12 12:54:57 +0100

    Initial Commit second

--- pierceoneillcv/urls.py
+++ pierceoneillcv/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import path,include
 from django.views.generic import RedirectView
 from django.contrib import admin
 from home.views import get_index
