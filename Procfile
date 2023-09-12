71993e06505512c3b1623bbc8bae41aa01a053d1
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-12 12:29:11 +0100

    Initial Commit second

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pierceoneillcv.wsgi:application
---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from blog.models import Post
+
+def get_index(request):
+    posts = Post.objects.order_by('-published_date').filter()[:3]
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)
